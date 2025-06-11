bedb2c8a59ce24254cff5caf446c9c362e1b68da
Pankaj <pankaj@avioxtechnologies.com>
2025-06-11 17:49:06 +0530

    Added Anyone Sharing section

--- apps/client/src/pages/upload-resume/LoadingResume.tsx
+++ apps/client/src/pages/upload-resume/LoadingResume.tsx
@@ -39,7 +39,6 @@ const LoadingResume = ({ isComplete }: LoadingResumeProps) => {
       </div>
 
       <motion.div
-        key={currentIcon}
         initial={{ opacity: 0, y: 20 }}
         animate={{ opacity: 1, y: 0 }}
         exit={{ opacity: 0, y: -20 }}
